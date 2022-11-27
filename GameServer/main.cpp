8b4065d34eedea97c786c74a4dda1a1560cdaef7
index1207 <han93851207@gmail.com>
2022-11-27 18:07:38 +0900

    Thread

--- GameServer/main.cpp
+++ GameServer/main.cpp
@@ -2,6 +2,21 @@
 #include <iostream>
 #include "CorePch.h"
 
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <future>
+#include <Windows.h>
+
 int main() {
-	helloWord();
+	atomic<int64> num;
+	cout << boolalpha << num.is_lock_free() << '\n';
+
+	struct A {
+		int32 a;
+		int32 b;
+		int32 c;
+	};
+	atomic<A> a;
+	cout << boolalpha << a.is_lock_free() << '\n';
 }
