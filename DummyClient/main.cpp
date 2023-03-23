5282a08f446da3ecde7ed0f02863650b64ff579b
한태현 <s22035@gsm.hs.kr>
2023-03-23 19:30:00 +0900

    Select model

--- DummyClient/main.cpp
+++ DummyClient/main.cpp
@@ -1,5 +1,88 @@
 #include "pch.h"
+#include <iostream>
 
-int main() {
+#include <winsock2.h>
+#include <mswsock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+void HandleError(string cause)
+{
+	int32 errCode = ::WSAGetLastError();
+	cout << "[ERROR] Code : " << errCode << '\n';
+}
+
+int main()
+{
+	WSADATA wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		return -1;
+	}
+
+	SOCKET s = socket(PF_INET, SOCK_STREAM, 0);
 	
+	u_long isBlocking = false;
+	ioctlsocket(s, FIONBIO, &isBlocking);
+
+	SOCKADDR_IN addr;
+	addr.sin_family = AF_INET;
+	inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
+	addr.sin_port = htons(1225);
+
+	while (true)
+	{
+		if (::connect(s, (SOCKADDR*)&addr, sizeof(addr)) == SOCKET_ERROR)
+		{
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+			if (::WSAGetLastError() == WSAEISCONN)
+				break;
+		}
+	}
+
+	cout << "connected!\n";
+
+	char buf[100] = "Hello World!";
+
+	while (true)
+	{
+		if (::send(s, buf, sizeof(buf), NULL) == SOCKET_ERROR)
+		{
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+
+			// Error
+			break;
+		}
+		cout << "Send Data! (" << sizeof(buf) << "\n";
+
+		while (true)
+		{
+			char recvBuf[1000] = "";
+			int32 recvlen = ::recv(s, recvBuf, sizeof(recvBuf), NULL);
+			if (recvlen == SOCKET_ERROR)
+			{
+				if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				{
+					continue;
+				}
+
+				// Error
+				break;
+			}
+			else if (recvlen == 0)
+			{
+				// Disconnected
+				break;
+			}
+			cout << "Recv Data! (" << recvlen << ")\n";
+			break;
+		}
+
+		this_thread::sleep_for(1s);
+	}
+
+	::closesocket(s);
+	::WSACleanup();
 }
--- GameServer/main.cpp
+++ GameServer/main.cpp
@@ -1,30 +1,103 @@
 #include "pch.h"
+
 #include <iostream>
 
-using TL = TypeList<class Player, class Knight, class Archer>;
+#include <winsock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
 
-class Player
+#pragma comment(lib, "ws2_32.lib")
+
+const int BUFSIZE = 1024;
+
+struct Session
 {
-public:
-	CASTABLE_CLASS;
-public:
-	Player() { INIT_ID(Player); }
+	SOCKET sock = INVALID_SOCKET;
+	char recvBuf[BUFSIZE] = "";
+	int32 recvBytes = 0;
+	int32 sendBytes = 0;
 };
 
-class Knight : public Player
+void HandleError(string cause)
 {
-public:
-	Knight() { INIT_ID(Knight); }
-};
-
-class Archer : public Player
-{
-public:
-	Archer() { INIT_ID(Archer); }
-};
+	int32 errCode = ::WSAGetLastError();
+	cout << "[ERROR] Code : " << errCode << '\n';
+}
 
 int main()
 {
-	Player* p = new Knight();
-	Knight* k = checked_cast<Knight*>(p);
+	WSADATA wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData))
+	{
+		HandleError("Init");
+	}
+
+	SOCKET sock = socket(PF_INET, SOCK_STREAM, 0);
+
+	SOCKADDR_IN addr;
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sin_port = htons(1225);
+
+	::bind(sock, (SOCKADDR*)&addr, sizeof(addr));
+	::listen(sock, SOMAXCONN);
+
+	vector<Session> v;
+	v.reserve(100);
+
+	fd_set read;
+	fd_set write;
+	while (true)
+	{
+		FD_ZERO(&read);
+		FD_ZERO(&write);
+
+		FD_SET(sock, &read);
+
+		for (Session& s : v)
+		{
+			if (s.recvBytes <= s.sendBytes)
+				FD_SET(s.sock, &read);
+			else FD_SET(s.sock, &write);
+		}
+
+		if (::select(NULL, &read, &write, nullptr, nullptr) == SOCKET_ERROR)
+			break;
+		
+		if (FD_ISSET(sock, &read))
+		{
+			SOCKADDR_IN clientAddr;
+			int32 addrlen = sizeof(clientAddr);
+			SOCKET clientSock = ::accept(sock, (SOCKADDR*)&addrlen, &addrlen);
+			if (clientSock != INVALID_SOCKET)
+			{
+				cout << "Client Connected!\n";
+				v.push_back(Session{clientSock});
+			}
+		}
+
+		for (Session& s : v)
+		{
+			if (FD_ISSET(s.sock, &read))
+			{
+				int32 recvlen = ::recv(s.sock, s.recvBuf, BUFSIZ, NULL);
+				if (recvlen <= 0) continue;
+
+				s.recvBytes = recvlen;
+			}
+			if (FD_ISSET(s.sock, &write))
+			{
+				int32 sendlen = ::send(s.sock, &s.recvBuf[s.recvBytes], s.recvBytes - s.sendBytes, NULL);
+				if (sendlen == SOCKET_ERROR) continue;
+				
+				s.sendBytes += sendlen;
+				if (s.recvBytes == s.sendBytes)
+				{
+					s.recvBytes = s.sendBytes = 0;
+				}
+			}
+		}
+	}
+
+	::WSACleanup();
 }
